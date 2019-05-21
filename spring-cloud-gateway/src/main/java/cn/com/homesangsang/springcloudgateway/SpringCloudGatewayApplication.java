efc21848e738637b57edf7a68003e5250dc9b451
sangzhiyong <sangzhiyong@vtstar.net>
2019-05-21 20:40:44 +0800

    添加spring-cloud-gateway

---
+++ spring-cloud-gateway/src/main/java/cn/com/homesangsang/springcloudgateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,25 @@
+package cn.com.homesangsang.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+
+    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.host("**baidu**").uri("http://httpbin.org/").id("sa"))
+                .build();
+    }
+
+}
---
+++ spring-cloud-gateway/src/main/java/cn/com/homesangsang/springcloudgateway/config/RouteConfig.java
@@ -0,0 +1,5 @@
+package cn.com.homesangsang.springcloudgateway.config;
+
+public class RouteConfig {
+
+}
